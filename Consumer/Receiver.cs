5990e34b21f1fc1032ad942b500e2535f7e4d102
Hebert Costa Andrade <hcostapuc@gmail.com>
2020-02-18 20:18:17 -0300

    Add project files.

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Receiver
+    {
+        static void Main()
+        {
+            //Aqui nos conectamos a uma máquina local, daí o localhost.
+            //Se quisermos nos conectar uma máquina diferente, simplesmente especificaremos seu nome ou endereço IP.
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            //Aqui declaramos a fila. A mesma só será criada se já não existir.
+            channel.QueueDeclare(queue: "RabbitMQTest",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            //Agora adicionamos a parte do consumidor, que irá pegar as mensagens da fila:
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += Consumer_Received;
+            channel.BasicConsume(queue: "RabbitMQTest", autoAck: true, consumer: consumer);
+            Console.WriteLine("Consumer Working!");
+            //Usamos um console ReadLine, para manter a aplicação rodando. 
+            //Nosso consumer vai estar pegando toda mensagem que chega na fila RabbitMQTest e nos mostrando o que ela contém.
+            Console.ReadLine();
+
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body;
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Publisher/Sender.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Publisher
+{
+    class Sender
+    {
+        static void Main()
+        {
+            //Aqui nos conectamos a uma máquina local, daí o localhost.
+            //Se quisermos nos conectar uma máquina diferente, simplesmente especificaremos seu nome ou endereço IP.
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            //Aqui declaramos a fila. A mesma só será criada se já não existir.
+            channel.QueueDeclare(queue: "RabbitMQTest",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            const string message = "Tutorial RabbitMQ!";
+
+            //O conteúdo da mensagem é uma matriz de bytes, para que você possa codificar o que quiser.
+            var body = Encoding.UTF8.GetBytes(message);
+
+            //Aqui iremos adicionar o método para publicar a mensagem
+            channel.BasicPublish(exchange: string.Empty,
+                routingKey: "RabbitMQTest",
+                basicProperties: null,
+                body: body);
+            Console.WriteLine("Message Sent!");
+
+        }
+    }
+}
