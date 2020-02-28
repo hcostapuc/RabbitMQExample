894bbe160851eb8b6a5c98e2f8ed56da8cb386db
Hebert Costa Andrade <hcostapuc@gmail.com>
2020-02-27 21:38:47 -0300

    Adicionando Projetos:        TestExchangeDirect        TestExchangeFanout

---
+++ TestExchangeFanout/EmitLogFanout/EmitLog.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace EmitLogFanout
+{
+    class EmitLog
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            //aqui criamos o exchange logs do tipo Fanout
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+            var message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "logs",
+                                 routingKey: "",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0)
+                   ? string.Join(" ", args)
+                   : "info: Hello World!");
+        }
+    }
+}
---
+++ TestExchangeFanout/ReceiveLogsFanout/ReceiveLogs.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace ReceiveLogsFanout
+{
+    class ReceiveLogs
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+
+            //aqui criamos uma queue que será temporaria e de nome aleatório, assim que a aplicação fechar
+            //a queue é apagada do server
+            var queueName = channel.QueueDeclare().QueueName;
+
+            //baindamos com o exchange criado no EmitLog com a nossa queue
+            //para o fanout o parametro routingKey é irrelevante, por trabalhar como um broadcast ele sempre irá
+            //enviar para as filas que baindarem o exchange criado, assim o deixa em desuso.
+            channel.QueueBind(queue: queueName,
+                              exchange: "logs",
+                              routingKey: "");
+
+            Console.WriteLine(" [*] Waiting for logs.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] {0}", message);
+            };
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
